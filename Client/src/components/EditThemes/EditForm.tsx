3dd15edbf96b0a15d07bdb398366bb0fcd078717
Massimiliano Cristaldi <cristaldi.massimiliano@gmail.com>
2024-09-13 13:15:51 +0200

    fixed an issue that caused the color inputs in the edit theme tab to not take the correct default value, and made the interface prettier

--- Client/src/components/EditThemes/EditForm.tsx
+++ Client/src/components/EditThemes/EditForm.tsx
@@ -1,64 +1,78 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { defaultTheme, hexToHslString, getLightnessFromHex, formatColorProperty } from "../../ColorTools";
+import { ContactListRefContext, isMobile } from "../../utils";
 import { StyleProperties } from "../../types";
 import "./EditForm.css";
 
 export default function EditForm(){
 
-    const [colorProperties, setColorProperties] = useState<StyleProperties>(defaultTheme);
+    const [currentTheme, setCurrentTheme] = useState<StyleProperties>(()=>{
+        const savedTheme = window.localStorage.getItem("theme");
+        return savedTheme ? JSON.parse(savedTheme) : defaultTheme;
+    });
+    const [contactListRef, chatWrapperRef, backButtonRef] = useContext(ContactListRefContext);
+
+    useEffect(()=>{
+        if (isMobile()) {
+            contactListRef.current.style.display = "none";
+            chatWrapperRef.current.style.display = "block";
+            backButtonRef.current.style.visibility = "visible";
+        }
+    }, [])
 
     function setProperty(e: React.ChangeEvent<HTMLInputElement>){
         if (e.target.name === "iconBg") {
-            //This constant calculates the hsl lightness adjustment for button:hovers automatically, giving a darker shade
-            //if the color is light and a lighter shade if the color is dark 
+            //This constant calculates the hsl lightness adjustment for button:hovers automatically, giving
+            //a darker shade if the color is light and a lighter shade if the color is dark 
             const lightnessAdjust = getLightnessFromHex(e.target.value) as number <= 20 ? 10 : -10;
-            setColorProperties({...colorProperties,
+            setCurrentTheme({...currentTheme,
                 iconBg: e.target.value,
                 iconHoverBg: hexToHslString(e.target.value, lightnessAdjust) as string
             });
         } else {
-            setColorProperties({...colorProperties, [e.target.name]: e.target.value});
+            setCurrentTheme({...currentTheme, [e.target.name]: e.target.value});
         }
-        console.log("Value of colorProperties changed");
-        
     }
 
-    function saveThemeToLocalStorage(){
-        window.localStorage.setItem("theme", JSON.stringify(colorProperties));
-        console.log("Theme saved to local storage");
+    function saveTheme(theme:StyleProperties){
+        window.localStorage.setItem("theme", JSON.stringify(theme));
+        window.location.reload();
     }
 
+    //TODO: Add a popup menu to confirm reset, and maybe one to confirm save
     function resetDefaultTheme(){
-        setColorProperties(defaultTheme);
-        saveThemeToLocalStorage();
-        console.log("Default theme restored");
+        setCurrentTheme(defaultTheme);
+        saveTheme(defaultTheme);
     }
     
+    //TODO: Add the ability to change border radius and font family
     return(
         <div id="editThemeWrapper">
-            {Object.keys(colorProperties).map((el)=>(
+            {Object.keys(currentTheme).map((el)=>(
                 el !== "iconHoverBg" &&
                 <div key={el}>
-                    <label htmlFor={el} className="text-black">{formatColorProperty(el)}</label>
+                    <label htmlFor={el}>{formatColorProperty(el)}</label>
                     <input type="color"
                     name={el}
                     id={el}
                     onBlur={setProperty}
-                    defaultValue={colorProperties[el]}/>
+                    defaultValue={currentTheme[el]}/>
                 </div>
             ))}
-            <button className="text-black" onClick={saveThemeToLocalStorage}>
-                Apply changes
-            </button>
-            <button className="text-black" onClick={resetDefaultTheme}>
-                Reset default theme
-            </button>
-            <button className="text-black" onClick={()=>{console.log(colorProperties)}}>
-                Log colorProperties
-            </button>
-            <button className="text-black" onClick={()=>{console.log(window.localStorage.getItem("theme"))}}>
-                Log localStorage
-            </button>
+            <div id="editThemeButtonWrapper">
+                <button onClick={()=>saveTheme(currentTheme)}>
+                    Apply changes
+                </button>
+                <button onClick={resetDefaultTheme}>
+                    Reset default theme
+                </button>
+                <button onClick={()=>{console.log(currentTheme)}}>
+                    Log currentTheme
+                </button>
+                <button onClick={()=>{console.log(window.localStorage.getItem("theme"))}}>
+                    Log localStorage
+                </button>
+            </div>
         </div>
     )
 }
--- Client/src/components/Layout.tsx
+++ Client/src/components/Layout.tsx
@@ -8,27 +8,25 @@ import "../index.css";
 
 export default function Layout(){
 
-    const contact_list_ref = useRef<HTMLDivElement>(null);
-    const chat_wrapper_ref = useRef<HTMLDivElement>(null);
-    const back_button_ref = useRef<HTMLElement>(null);
+    const contactListRef = useRef<HTMLDivElement>(null);
+    const chatWrapperRef = useRef<HTMLDivElement>(null);
+    const backButtonRef = useRef<HTMLElement>(null);
 
     useEffect(()=>{
         const styles = document.documentElement.style;
         const theme = window.localStorage.getItem("theme");
         if (theme) {
             const colorProperties = JSON.parse(theme);
-            console.log(colorProperties);
             for (const property in colorProperties) {
                 if (Object.prototype.hasOwnProperty.call(colorProperties, property)) {
                     styles.setProperty(`--${property}`, colorProperties[property]);
                 }
             }
-        console.log(":root values changed");
         }
     }, [])
 
     return(
-        <ContactListRefContext.Provider value={[contact_list_ref, chat_wrapper_ref, back_button_ref]}>
+        <ContactListRefContext.Provider value={[contactListRef, chatWrapperRef, backButtonRef]}>
         <div className="container-fluid h-100">
             <div className="row">
                     <Toolbar/>
