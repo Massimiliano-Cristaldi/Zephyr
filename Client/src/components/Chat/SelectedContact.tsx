7a8325c3dbf4b0115b594eca02ffe54d470f2d00
Massimiliano Cristaldi <cristaldi.massimiliano@gmail.com>
2024-09-16 18:11:40 +0200

    started implementing the logic to check whether or not a user is in your contact list when you navigate to the url corresponding to that user's id; todo: params should use phone number, not user id

--- Client/src/components/Chat/SelectedContact.tsx
+++ Client/src/components/Chat/SelectedContact.tsx
@@ -6,11 +6,13 @@ import ChatWindow from "./ChatWindow";
 import "./Body.css";
 import { useContext, useEffect, useState } from "react";
 import { ContactListRefContext, isMobile } from "../../utils";
+import UserNotAdded from "./UserNotAdded";
 
 export default function SelectedContact(){
     
     const params = useParams();    
     const [contact, setContact] = useState<User>({id: 0, username: "N/A", phone_number: 0, icon_url: "/user.png"});
+    const [userIsAdded, setUserIsAdded] = useState(true);
     const [contactListRef, chatWrapperRef, backButtonRef] = useContext(ContactListRefContext)
 
     useEffect(()=>{
@@ -25,16 +27,14 @@ export default function SelectedContact(){
         async function fetchData(){
             try {
                 const response = await axios.get(`http://localhost:8800/contact/${params.contactId}`);
-                // const contacts = await axios.get(`http://localhost:8800/contactlist/${params.authId}`);
-                // console.log(contacts.data);
-                // console.log(response.data[0]);
-                
-                // const isInContactList = contacts.data.includes(response.data[0]);
-                // console.log(isInContactList);
-                
+                const contacts = await axios.get(`http://localhost:8800/contactlist/${params.authId}`);                
+                const isInContactList = contacts.data.some((obj:User) => obj.id === Number(params.contactId));                
                 if (response.status !== 200 || response.data?.length === 0) {
                     throw new Error("Fetch failed");
-                }        
+                }
+                if (!isInContactList) {
+                    setUserIsAdded(false);
+                }
                 setContact(response.data[0]);
             } catch (err) {
                 console.error(err);
@@ -45,6 +45,7 @@ export default function SelectedContact(){
     
     return(
         <>
+            {userIsAdded || <UserNotAdded/>}
             <ChatToolbar contact={contact}/>
             <ChatWindow/>
         </>
---
+++ Client/src/components/Chat/UserNotAdded.tsx
@@ -0,0 +1,9 @@
+export default function UserNotAdded(){
+    return(
+        <div id="userNotAddedWrapper">
+            <div id="userNotAddedModal">
+                This user is not in your contact list yet. Would you like to add them?
+            </div>
+        </div>
+    )
+}
